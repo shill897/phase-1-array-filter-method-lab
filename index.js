393cee3cdbd411dc181c869dbd6c99ec87d2a53f
mohamedshill <mohammedabdulahi248@gmail.com>
2023-12-30 20:27:33 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  
+  const driversData = [
+    { name: "John" },
+    { name: "Jane" },
+    { name: "Mike" },
+    
+  ];
+  
+  
+  const matchedDrivers = findMatching(driversData, "John");
+  console.log("Matching Drivers:", matchedDrivers);
+  
+  const fuzzyMatchedDrivers = fuzzyMatch(driversData.map(driver => driver.name), "Ja");
+  console.log("Fuzzy Matching Drivers:", fuzzyMatchedDrivers);
+  
+  const nameMatchedDrivers = matchName(driversData, "Mike");
+  console.log("Name Matching Drivers:", nameMatchedDrivers);
+  
